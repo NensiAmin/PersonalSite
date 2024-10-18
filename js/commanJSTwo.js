532f15e617b5efb799155f6e61487856a753f4bf
Parth <parth@akashganga.in>
2024-10-18 12:27:58 +0530

    Reinitialize in new directory

---
+++ js/commanJSTwo.js
@@ -0,0 +1,4 @@
+// Common JavaScript for Skills, Testimonials, Hobbies Pages
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript Loaded for Skills, Testimonials, and Hobbies Pages');
+});
